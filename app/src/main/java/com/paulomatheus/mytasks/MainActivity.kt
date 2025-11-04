74155fb60a21007b015e3f46483abacedfdcaa87
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-11-04 14:47:42 -0300

    feat: enable view binding and update project configuration

--- app/src/main/java/com/paulomatheus/mytasks/MainActivity.kt
+++ app/src/main/java/com/paulomatheus/mytasks/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.paulomatheus.mytasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
